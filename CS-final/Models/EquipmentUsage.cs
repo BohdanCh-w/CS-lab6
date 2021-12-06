d64d56f08d875c2a77a7311c6b2c6aa8fdca637a
Bohdan Ch <BohdanCh.the.One@gmail.com>
2021-12-06 22:58:22 +0200

    Add tables

---
+++ CS-final/Models/EquipmentUsage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CS_final.Models {
+    public class EquipmentUsage {
+        [Key]
+        public long EquipmentUsageID { get; set; }
+
+        public int GameID { get; set; }
+        public Game Game { get; set; }
+
+        public int EquipmentID { get; set; }
+        public Equipment Equipment { get; set; }
+
+        public int Number { get; set; }
+
+        public decimal SnapPrice { get; set; }
+    }
+}
---
+++ CS-final/Models/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CS_final.Models {
+    public class Game {
+        [Key]
+        public long GameID { get; set; }
+
+        public int CustomerID { get; set; }
+        public Customer Customer { get; set; }
+
+        public int LocationID { get; set; }
+        public Location Location { get; set; }
+
+        public int PlayersCount { get; set; }
+    }
+}
